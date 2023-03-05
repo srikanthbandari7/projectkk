097fb99cdeadfd2c7748fae334cee0f4716e4963
srikanthbandari7 <srikanthbandari2508@gmail.com>
2023-03-05 21:08:35 +0530

    updating changes

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,59 +1,79 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
+export default function Signup(props) {
+  const url = `http://localhost:3009/userdata`;
 
-export default function Signup(props){
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    confirm: "",
+  });
 
-    const url =`http://localhost:3009/userdata`;
+  let { name, email, phone, confirm } = data;
 
-    const [data, setData] = useState({
-        name:'',
-        email:'',
-        phone:'',
-        confirm:''
-    })
+  let formHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-    let {name, email, phone, confirm} = data;
+  let submitHandler = (e) => {
+    e.preventDefault();
 
-    let formHandler=(e)=>{
-        setData({...data, [e.target.name]:e.target.value})
-    }
+    let formData = {
+      name: `${data.name}`,
+      email: `${data.email}`,
+      phone: `${data.phone}`,
+    };
 
-    let submitHandler=(e)=>{
-        e.preventDefault();
+    axios.post(url, formData)
+     .then(()=>props.history.push('/Userdata'));
+  };
 
-        let formData = {
-
-            name:`${data.name}`,
-            email:`${data.email}`,
-            phone:`${data.phone}`
-        }
-      alert(JSON.stringify(formData))
-       
-      axios.post(url, formData).then(()=>props.history.push('/Userdata'))
-    }
-
-    return(
-        <div className='container my-5' >
-            <h4>REGISTRATION FORM</h4>
-         <form onSubmit={submitHandler.bind(this)} >
-          <div className='mb-3' >
-            <label className='form-label' >NAME :</label>
-            <input className='form-control' type='text' value={name} name='name' onChange={formHandler.bind(this)} />
-          </div>
-          <div className='mb-3' >
-            <label className='form-label' >EMAIL :</label>
-            <input className='form-control' type='email' value={email} name='email' onChange={formHandler.bind(this)} />
-          </div>
-          <div className='mb-3' >
-            <label className='form-label' >PHONE :</label>
-            <input className='form-control' type='number' value={phone} name='phone' onChange={formHandler.bind(this)} />
-          </div>
-          <div className='mb-3' >
-            <button type='submit'className='btn btn-success' value='update' >CREATE ACCOUNT</button>
-          </div>
-         </form>
-         <p className='text-success' >{confirm}</p>
+  return (
+    <div className="container p-3">
+      <h4 className="text-center" >REGISTRATION FORM</h4>
+      <div className="container w-50" >
+      <form onSubmit={submitHandler.bind(this)}>
+        <div className="mb-3">
+          <label className="form-label">NAME :</label>
+          <input
+            className="form-control"
+            type="text"
+            value={name}
+            name="name"
+            onChange={formHandler.bind(this)}
+          />
         </div>
-    )
+        <div className="mb-3">
+          <label className="form-label">EMAIL :</label>
+          <input
+            className="form-control"
+            type="email"
+            value={email}
+            name="email"
+            onChange={formHandler.bind(this)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">PHONE :</label>
+          <input
+            className="form-control"
+            type="number"
+            value={phone}
+            name="phone"
+            onChange={formHandler.bind(this)}
+          />
+        </div>
+        <div className="mb-3">
+          <button type="submit" className="btn btn-success" value="update">
+            CREATE ACCOUNT
+          </button>
+        </div>
+      <p className="text-success">{confirm}</p>
+
+      </form>
+      </div>
+    </div>
+  );
 }
